1fb5cedc2b1051056369bee21aad68b63afeafbf
Gabi Mor <gabimor@gmail.com>
2019-02-28 13:08:49 +0200

    env setup

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-const withBundleAnalyzer = require("@zeit/next-bundle-analyzer")
+const { parsed: localEnv } = require("dotenv").config()
+const webpack = require("webpack")
 
-module.exports = withBundleAnalyzer({
-  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/server.html",
-    },
-    browser: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/client.html",
-    },
+module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+
+    return config
   },
-})
+}
--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,8 @@ import App, { Container } from "next/app"
 import Head from "next/head"
 import withReduxStore from "../helpers/withReduxStore"
 import { Provider } from "react-redux"
-import fetch from "isomorphic-unfetch"
-
-const isServer = typeof window === "undefined"
 
 class MyApp extends App {
-  // static async getInitialProps({ Component, router, ctx, req }) {
-  //   // TODO: set the current domain
-  //   // TODO: t  his loads on every page render, should load once
-  //   if (isServer) {
-  //     // const baseUrl = 'http://localhost:' + +process.env.PORT
-  //     const baseUrl = "http://localhost:3000"
-  //     let pageProps = await fetch(`${baseUrl}/api/data`).then(res => res.json())
-  //     return { ...pageProps }
-  //   }
-  // }
-
   render() {
     const { Component, pageProps, reduxStore } = this.props
 
