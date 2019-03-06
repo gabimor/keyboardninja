a41c4a3b48d63a9949eb2630b9b59623dd8cb950
Gabi Mor <gabimor@gmail.com>
2019-03-06 17:58:39 +0200

    upgrade to storybook 5

--- pages/layout/layout.stories.js
+++ pages/layout/layout.stories.js
@@ -3,10 +3,8 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 
 import Logo from "./Logo"
-import Header from "./Header"
 import Footer from "./Footer"
 
 storiesOf("layout", module)
   .add("Logo", () => <Logo />)
-  .add("Header", () => <Header />)
   .add("Footer", () => <Footer />)
