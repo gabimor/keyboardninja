a55a3e1090fca8b1ed152485cf8629c0d14379d6
Gabi Mor <gabimor@gmail.com>
2019-04-10 22:30:50 +0300

    new files

---
+++ src/client/pages/app/GetLink.js
@@ -0,0 +1,18 @@
+import React, { useContext } from "react" // eslint-disable-line no-unused-vars
+import styled from "@emotion/styled"
+
+
+const GetLink = ({ hash }) => {
+  
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default GetLink
+
+const Container = styled.div`
+  background:black;  
+`
---
+++ src/client/pages/app/GetLink.stories.js
@@ -0,0 +1,9 @@
+import React from "react" // eslint-disable-line no-unused-vars
+
+import { storiesOf } from "@storybook/react"
+
+import GetLink from "./GetLink"
+
+storiesOf("GetLink", module).add("default", () => (
+  <GetLink hash="5cae18b7487b851b1c7210ad" />
+))
