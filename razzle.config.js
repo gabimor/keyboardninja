f4000d5728991f69248a7f94bf262a4501ca9dd6
Gabi <gabimor@gmail.com>
2019-04-18 13:41:51 +0300

    razzle-heroku

--- razzle.config.js
+++ razzle.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  modify(config, { target, dev }, webpack) {
-    config.devtool = dev ? "eval-source-map" : "none"
+  modify: require("razzle-heroku"),
+  // modify(config, { target, dev }, webpack) {
+  //   config.devtool = dev ? "eval-source-map" : "none"
 
-    console.log("config")
-    console.log(config)
-    console.log("target")
-    console.log(target)
-    console.log("dev")
-    console.log(dev)
-    console.log("webpack")
-    console.log(webpack)
-    return config
-  },
+  //   console.log("config")
+  //   console.log(config)
+  //   console.log("target")
+  //   console.log(target)
+  //   console.log("dev")
+  //   console.log(dev)
+  //   console.log("webpack")
+  //   console.log(webpack)
+  //   return config
+  // },
 }
