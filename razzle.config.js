b2671eef23b3357513ad6dc538e93fabdc29c6c9
Gabi Mor <gabi.mor@jolt.io>
2020-03-05 09:46:36 +0200

    .gitignore fix

--- razzle.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  modify: require("razzle-heroku"),
-  // modify(config, { target, dev }, webpack) {
-  //   config.devtool = dev ? "eval-source-map" : "none"
-  // },
-}
--- src/client/pages/app/GetLinkPopup.stories.js
+++
@@ -1,9 +0,0 @@
-import React from "react" // eslint-disable-line no-unused-vars
-
-import { storiesOf } from "@storybook/react"
-
-import GetLinkPopup from "./GetLinkPopup"
-
-storiesOf("GetLinkPopup", module).add("default", () => (
-  <GetLinkPopup link="http://keyboardninja.me/photoshop/d1fcv3qaasc3421" />
-))
