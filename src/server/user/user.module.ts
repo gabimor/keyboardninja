328850b4e5e63f8f795b4d8ffad1a00bb0783975
Gabi Mor <gabi.mor@jolt.io>
2020-08-23 17:16:54 +0300

    solved DI bug

--- src/server/user/user.module.ts
+++ src/server/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "@server/db/User.schema";
 import { UserService } from "./user.service";
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UserService],
   exports: [UserService],
 })
