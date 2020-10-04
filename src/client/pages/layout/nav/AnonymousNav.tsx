470a0a3c49e43f73c9c6f356bf495d7eae5de929
Gabi Mor <gabi.mor@jolt.io>
2020-10-04 23:22:39 +0300

    trying hook

--- src/client/pages/layout/nav/AnonymousNav.tsx
+++ src/client/pages/layout/nav/AnonymousNav.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { PrimaryButton } from "../../../components/Buttons";
 
 export default function Anonymous() {
+  const a = 1;
+
   return (
     <ul>
       <li>
